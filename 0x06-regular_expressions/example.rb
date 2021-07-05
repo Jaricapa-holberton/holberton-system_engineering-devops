a5413da1ae2fb6e3bc804a4ddf11d0899ecb97f5
Jaricapa-holberton <2241@holbertonschool.com>
2021-07-05 17:02:25 -0500

    update files and task 7 v2

--- 0x06-regular_expressions/example.rb
+++ 0x06-regular_expressions/example.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
+"""
+sylvain@ubuntu$ ./example.rb 127.0.0.2
+127.0.0.2
+sylvain@ubuntu$ ./example.rb 127.0.0.1
+127.0.0.1
+sylvain@ubuntu$ ./example.rb 127.0.0.a
+"""
 puts ARGV[0].scan(/127.0.0.[0-9]/).join
